b02c4f6a251b00c302375e31c5168fe32317286e
Conor Ring <ringconor73@gmail.com>
2025-03-13 21:59:08 +0000

    Added some comments explaining the use of extern keyword

--- Chapter_7/internalLinkage/main.cpp
+++ Chapter_7/internalLinkage/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 // extern lets compiler know that g_x is defined elsewhere
+// global variables have external linkage by default. If we were to get rid of the extern keyword
+// the compiler would think we are declaring a new variable which would clash with the one in linkage.cpp
 extern int g_x;
 
 // static shields this function definition from the linker so no error occurs
