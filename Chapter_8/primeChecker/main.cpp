b89ef79edc8846db9f6d163ecbd327389b22d1ff
Conor Ring <ringconor73@gmail.com>
2025-05-12 18:39:21 +0100

    Finished chapter 8. Some quizzes done

---
+++ Chapter_8/primeChecker/main.cpp
@@ -0,0 +1,50 @@
+// make sure assert triggers even if we compile in release mode
+#undef NDEBUG
+
+#include <cassert> // for assert
+#include <iostream>
+#include <stdexcept>
+#include <cmath>
+
+bool isPrime(int x) {
+
+    if (x < 0) {
+        throw std::invalid_argument("Input must be a positive integer.");
+    }
+
+    if( x == 0 ) return false;
+    if( x == 1 ) return false;
+    if( x == 2 ) return true;
+    if( x%2 == 0) return false;
+
+    for( int i{3}; i <= pow(x, 0.5); i+=2) {
+        if( x%i == 0 ) return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    assert(!isPrime(0)); // terminate program if isPrime(0) is true
+    assert(!isPrime(1));
+    assert(isPrime(2));  // terminate program if isPrime(2) is false
+    assert(isPrime(3));
+    assert(!isPrime(4));
+    assert(isPrime(5));
+    assert(isPrime(7));
+    assert(!isPrime(9));
+    assert(isPrime(11));
+    assert(isPrime(13));
+    assert(!isPrime(15));
+    assert(!isPrime(16));
+    assert(isPrime(17));
+    assert(isPrime(19));
+    assert(isPrime(97));
+    assert(!isPrime(99));
+    assert(isPrime(13417));
+
+    std::cout << "Success!\n";
+
+    return 0;
+}
